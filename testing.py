a8194c1f7871152cfe6d4340091fcbe979f649ad
natgho-reidin <sbozkir@reidin.com>
2016-11-26 01:56:07 +0300

    Complete Data class structure

--- testing.py
+++ testing.py
@@ -1,19 +1,11 @@
 import pprint
-
 from wolfstreet import *
-# for all data print dump style... from pprint import pprint
-data = get_specific_data(5)
-ana_mekan = Currencies()
-# if you want print all data list type
-for date, currencies in data.iteritems():
-    # print "Tarih:", date
-    ana_mekan.add(currencies)
-    for current, pay in currencies.iteritems():
-        print current, pay
-# for sort by date : sorted(dct.items(), key=lambda p: p[0], reverse=True)
 
-from wolfstreet import *
+
 total_currencies = Currencies(5)
-
 total_currencies.calculate_correlation()
+CL_currencies = total_currencies.calculate_currencies_correlation()
 
+# Print all class content
+for currentss in CL_currencies:
+    print currentss.name,currentss.corelation,currentss.all_data
